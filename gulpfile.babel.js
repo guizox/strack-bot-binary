a6fc6cd8f52e489831051074df5a8e3da347d2e4
Vitor Vieira <vitor.vieira@skyone.solutions>
2019-12-05 11:32:28 -0300

    s

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,98 +1,101 @@
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const ghpages = require('gh-pages');
-const connect = require('gulp-connect');
-const open = require('gulp-open');
-require('./gulp/i18n');
-require('./gulp/build');
-require('./gulp/plato');
+const gulp = require("gulp");
+const watch = require("gulp-watch");
+const ghpages = require("gh-pages");
+const connect = require("gulp-connect");
+const open = require("gulp-open");
+require("./gulp/i18n");
+require("./gulp/build");
+require("./gulp/plato");
 
 gulp.task(
-    'connect',
-    gulp.series(done => {
-        connect.server({
-            root      : 'www',
-            port      : 3000,
-            livereload: false,
+  "connect",
+  gulp.series(done => {
+    connect.server({
+      root: "www",
+      port: process.env.PORT || 3000,
+      livereload: false
+    });
+    done();
+  })
+);
+
+gulp.task(
+  "serve",
+  gulp.series("connect", done => {
+    done();
+  })
+);
+
+gulp.task(
+  "release-branch",
+  gulp.series(done => {
+    const index = process.argv.indexOf("--branch");
+    let option = "";
+    if (index <= -1) {
+      throw Error("Please specify branch");
+    } else {
+      option = process.argv[index + 1];
+    }
+    ghpages
+      .publish("www", {
+        dest: option,
+        add: true
+      })
+      .then(done);
+  })
+);
+
+gulp.task(
+  "release-master",
+  gulp.series(done => {
+    ghpages
+      .publish("./", {
+        src: ["404.md", "LICENSE", "README.md", "CNAME"],
+        add: true
+      })
+      .then(() => {
+        ghpages.publish("www", {
+          add: true
         });
-        done();
-    })
+      })
+      .then(done);
+  })
 );
 
 gulp.task(
-    'serve',
-    gulp.series('connect', done => {
-        done();
-    })
+  "test-deploy",
+  gulp.series("build-min", "serve", () => {})
 );
 
 gulp.task(
-    'release-branch',
-    gulp.series(done => {
-        const index = process.argv.indexOf('--branch');
-        let option = '';
-        if (index <= -1) {
-            throw Error('Please specify branch');
-        } else {
-            option = process.argv[index + 1];
-        }
-        ghpages
-            .publish('www', {
-                dest: option,
-                add : true,
-            })
-            .then(done);
-    })
+  "watch-static",
+  gulp.parallel(done => {
+    gulp.watch(
+      ["static/xml/**/*", "static/*.html", "static/css/*.scss"],
+      {
+        debounceTimeout: 1000
+      },
+      gulp.series("build-dev-static")
+    );
+    done();
+  })
 );
 
 gulp.task(
-    'release-master',
-    gulp.series(done => {
-        ghpages
-            .publish('./', {
-                src: ['404.md', 'LICENSE', 'README.md', 'CNAME'],
-                add: true,
-            })
-            .then(() => {
-                ghpages.publish('www', {
-                    add: true,
-                });
-            })
-            .then(done);
-    })
+  "watch-html",
+  gulp.series(done => {
+    console.log("------- watch html ----xxx----");
+    gulp.watch(
+      ["templates/**/*"],
+      {
+        debounceTimeout: 1000
+      },
+      gulp.series("build-dev-html")
+    );
+    done();
+  })
 );
 
-gulp.task('test-deploy', gulp.series('build-min', 'serve', () => {}));
+gulp.task("watch", gulp.series("build", "serve"));
 
-gulp.task(
-    'watch-static',
-    gulp.parallel(done => {
-        gulp.watch(
-            ['static/xml/**/*', 'static/*.html', 'static/css/*.scss'],
-            {
-                debounceTimeout: 1000,
-            },
-            gulp.series('build-dev-static')
-        );
-        done();
-    })
-);
-
-gulp.task(
-    'watch-html',
-    gulp.series(done => {
-        console.log('------- watch html ----xxx----');
-        gulp.watch(
-            ['templates/**/*'],
-            {
-                debounceTimeout: 1000,
-            },
-            gulp.series('build-dev-html')
-        );
-        done();
-    })
-);
-
-gulp.task('watch', gulp.series('build', 'serve'));
-
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("watch"));
