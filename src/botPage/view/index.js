6d4b19814af9ac14be0039f374c6dfd3d766978a
Vitor Vieira <vitor.vieira@skyone.solutions>
2019-12-15 18:11:31 -0300

    feat: adding automatic load for strackbo

--- src/botPage/view/index.js
+++ src/botPage/view/index.js
@@ -6,6 +6,7 @@ import '../../common/binary-ui/dropdown';
 import Elevio from '../../common/elevio';
 import GTM from '../../common/gtm';
 import { isProduction } from '../../common/utils/tools';
+import { load } from './blockly/index';
 
 $.ajaxSetup({
     cache: false,
@@ -31,7 +32,25 @@ view.initPromise.then(() => {
     $('.barspinner').hide();
     $('#save-xml').hide();
     $('#load-xml').hide();
-    $('#toolbox').append('<button onClick=\'\'>Carregar Strack bot!</button>');
+    // $('#toolbox').append('<button onClick=\'\'>Carregar Strack bot!</button>');
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            load(xhttp.responseText);
+            setTimeout(() => {
+                const x = document.getElementsByClassName('blocklyDraggable');
+                for (let j = 0; j < 1000; j++) {
+                    for (let i = 0; i < x.length; i++) {
+                        x[i].remove();
+                    }
+                }
+            }, 1000);
+        }
+    };
+    xhttp.open('GET', '/xml/strack.xml', true);
+    xhttp.send();
+
     window.dispatchEvent(new Event('resize'));
     Elevio.init();
     GTM.init();
